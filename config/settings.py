6dea62d6351e2f21b0ebb7e259c511072a0ba4aa
pedram <peeeedii@gmail.com>
2025-02-13 19:16:21 +0330

    feat: add dotenv package

--- config/settings.py
+++ config/settings.py
@@ -1,20 +1,14 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+load_dotenv(BASE_DIR / ".env")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
-
-print(DEBUG)
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
