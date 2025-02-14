ba860749192205a636961d9a94247f841571bc30
pedram <peeeedii@gmail.com>
2025-02-15 01:09:41 +0330

    chore: change the name of the login and register routes

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from .views import home_view, register_account, login_account
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("account/register", register_account, name="register_account"),
-    path("account/login", login_account, name="login_account"),
+    path("account/register", register_account, name="register"),
+    path("account/login", login_account, name="login"),
 ]
