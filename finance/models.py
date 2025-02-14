336ef8479135af667bbd42cdeb78f6c8265984aa
pedram <peeeedii@gmail.com>
2025-02-14 15:49:01 +0330

    refactor: move the token model to the core app

--- finance/models.py
+++ finance/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Token(models.Model):
-    token = models.CharField(max_length=255)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "{} - {}".format(self.user, self.token)
-
-
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     amount = models.BigIntegerField()
