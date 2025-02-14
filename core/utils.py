89fb768cfea809a34a78a8d2bdaf555e565c5c9f
pedram <peeeedii@gmail.com>
2025-02-14 19:28:43 +0330

    chore: remove punc from the charset for easy to use and dont set --data-urlencode in the curl command

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,7 @@
 from random import choices
 import string
 
-ascii_charset = string.ascii_letters + string.digits + string.punctuation
+ascii_charset = string.ascii_letters + string.digits
 
 
 def token_generator() -> str:
