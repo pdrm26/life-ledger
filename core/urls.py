4b30e7cd2976de4091b69df13665c90f89726847
pedram <peeeedii@gmail.com>
2025-02-14 15:49:43 +0330

    feat: add the user login and register

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, register_account, login_account
 
 
 urlpatterns = [
-    path("", home_view, name="home")
+    path("", home_view, name="home"),
+    path("account/register", register_account, name="register_account"),
+    path("account/login", login_account, name="login_account"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .utils import token_generator
+from django.contrib.auth.models import User
+from .models import Token
 
 
 def home_view(request):
     return render(request, "home.html")
+
+
+@csrf_exempt
+def register_account(request):
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    token = token_generator()
+
+    user = User.objects.create(
+        username=username, email=email, password=password)
+    Token.objects.create(token=token, user=user)
+
+    return JsonResponse({'status': "ok", "token": token})
+
+
+@csrf_exempt
+def login_account(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = User.objects.get(username=username, password=password)
+    token = Token.objects.get(user=user)
+
+    return JsonResponse({'status': 'ok', 'token': token.token, })
