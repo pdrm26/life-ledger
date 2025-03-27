45b6b291dc852cf68749e611441d3b94b74ca736
pedram <peeeedii@gmail.com>
2025-03-27 19:12:21 +0330

    fix: create the token in the save method to prevent multiple migration file every time makemigrate

--- core/models.py
+++ core/models.py
@@ -4,8 +4,13 @@ from django.utils.crypto import get_random_string
 
 
 class Token(models.Model):
-    token = models.CharField(max_length=255, default=get_random_string(length=48))
+    token = models.CharField(max_length=255, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = get_random_string(length=48)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return "{} - {}".format(self.user, self.token)
