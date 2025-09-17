503d378b1abcffbf5e0047895cf480cddf9219d1
pedram <peeeedii@gmail.com>
2025-09-17 23:44:52 +0330

    chore: text to desc name

--- finance/models.py
+++ finance/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 
 class Expense(models.Model):
-    text = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{} - {} - {}".format(self.date, self.text, self.amount)
+        return "{} - {} - {}".format(self.date, self.description, self.amount)
 
 
 class Income(models.Model):
-    text = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{} - {} - {}".format(self.date, self.text, self.amount)
+        return "{} - {} - {}".format(self.date, self.description, self.amount)
