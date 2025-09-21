b67ce99a497abe50e3cfab1af9c2a52ad7e759c7
pedram <peeeedii@gmail.com>
2025-09-21 21:34:28 +0330

    chore: remove deprecate client req

--- client/commandline/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-TOKEN="YOUR_TOKEN"
-BASE_URL=http://localhost:8000
--- client/commandline/expense.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source config.sh
-
-
-curl -X POST -H "Token: $TOKEN" --data "amount=$1&text=$2" http://localhost:8000/finance/submit/expense
