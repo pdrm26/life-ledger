3b2672b6cffff36fa079f4a8727e2266ef8324d7
pedram <peeeedii@gmail.com>
2025-09-22 21:41:48 +0330

    chore: change the home_page view name to home

--- finance/urls.py
+++ finance/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name="home_page"),
+    path("", views.home_page, name="home"),
     path("submit/expense", views.submit_expense, name="submit_expense"),
     path("submit/income", views.submit_income, name="submit_income"),
     path("q/generalstat", views.generalstat, name="generalstat"),
