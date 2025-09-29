c5195c3c8298ca37f3819d9e9705d77030908c32
pedram <peeeedii@gmail.com>
2025-09-29 22:52:14 +0330

    refactor: mix the income,expense models to transaction model

--- finance/models.py
+++ finance/models.py
@@ -2,21 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Expense(models.Model):
+class Transaction(models.Model):
+    class TransactionTypes(models.TextChoices):
+        INCOME = "income"
+        EXPENSE = "expense"
+
     description = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tx_type = models.CharField(choices=TransactionTypes, default="income", max_length=7)
 
     def __str__(self):
-        return "{} - {} - {}".format(self.date, self.description, self.amount)
-
-
-class Income(models.Model):
-    description = models.CharField(max_length=255)
-    amount = models.BigIntegerField()
-    date = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return "{} - {} - {}".format(self.date, self.description, self.amount)
+        return "{} -> {} - {} - {}".format(self.tx_type, self.date, self.description, self.amount)
--- finance/views.py
+++ finance/views.py
@@ -1,14 +1,12 @@
 from datetime import date
-from json import JSONEncoder
 
 from django.contrib.auth.models import User
-from django.db.models import Count, Sum
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import TransactionForm
-from .models import Expense, Income
+from .models import Transaction
 
 fake_transactions = [
     {
@@ -48,11 +46,7 @@ def home_page(request):
         user = get_object_or_404(User, id=request.user.id)
         form = TransactionForm(request.POST)
         if form.is_valid():
-            tx_type = form.cleaned_data.pop("tx_type")
-            if tx_type == "expense":
-                Expense.objects.create(user=user, **form.cleaned_data)
-            elif tx_type == "income":
-                Income.objects.create(user=user, **form.cleaned_data)
+            Transaction.objects.create(user=user, **form.cleaned_data)
     else:
         form = TransactionForm()
 
@@ -68,23 +62,23 @@ def home_page(request):
 
 @csrf_exempt
 def generalstat(request):
-    if request.method == "GET":
-        user = get_object_or_404(User, id=request.user.id)
+    # if request.method == "GET":
+    #     user = get_object_or_404(User, id=request.user.id)
 
-        user_total_expense = Expense.objects.filter(user=user).aggregate(
-            total_expense=Sum("amount", default=0), total_expense_count=Count("amount")
-        )
-        user_total_income = Income.objects.filter(user=user).aggregate(
-            total_income=Sum("amount", default=0), total_income_count=Count("amount")
-        )
+    #        user_total_expense = Expense.objects.filter(user=user).aggregate(
+    #            total_expense=Sum("amount", default=0), total_expense_count=Count("amount")
+    #        )
+    #        user_total_income = Income.objects.filter(user=user).aggregate(
+    #            total_income=Sum("amount", default=0), total_income_count=Count("amount")
+    #        )
 
-        return JsonResponse(
-            {
-                "status": "OK",
-                "expense": user_total_expense,
-                "income": user_total_income,
-                "balance": user_total_income["total_income"] - user_total_expense["total_expense"],
-            },
-            encoder=JSONEncoder,
-        )
+    # return JsonResponse(
+    #     {
+    #         "status": "OK",
+    #         "expense": user_total_expense,
+    #         "income": user_total_income,
+    #         "balance": user_total_income["total_income"] - user_total_expense["total_expense"],
+    #     },
+    #     encoder=JSONEncoder,
+    # )
     return JsonResponse({"error": "Only GET request are allowed"}, status=400)
